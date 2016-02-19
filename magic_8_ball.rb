994e5f637bd94c43778a13f3c68cf199b9863c86
Gavyn Caldwell <gavyncaldwell@gmail.com>
2016-02-18 20:28:43 -0700

    i turned case statements into hashes

--- magic_8_ball.rb
+++ magic_8_ball.rb
@@ -1,6 +1,12 @@
-#need to finish with resetting the answers
+@easter_egg = {
+  "what is your favorite band" => "yellow card",
+  "what is your favorite food" => "sushi",
+  "who do you love" => "Jessie, im going to marry her",
+  "what do you like to do" => "write this crazy code stuff",
+}
 
 @answers = ["well, duh....", "Dont ask me that", "are you kidding? NO!", "too high to think, ask again"]
+@user_answers = []
 
 def add
   puts "type a new answer"
@@ -13,35 +19,45 @@ def add
       puts "thank you come again"
       exit
     else
-      @answers << "#{new_answer}\n"
+      @user_answers << new_answer
     main_page
     end
 end
 
 def the_answer
-  answer = @answers.shuffle
-  message = answer[0]
+  answer = @answers + @user_answers
+  random_answer = answer.shuffle
+  message = random_answer[0]
   puts message
   main_page
 end
 
 def main_page
-  puts "\n\n---WELCOME TO MAGIC 8 BALL---\n1. ask a question!\n2. add answer\n3. reset answers\n4. list of answers\ntype quit in any moment to exit magic 8 ball"
-  response = gets.strip.downcase
-  case response
-  when "1"
-      puts "ask question"
-      ques = gets.strip
-      the_answer
-  when "2"
-      add
-  when "4"
-    puts @answers
-  when "quit"
+
+  puts "\n\n---WELCOME TO MAGIC 8 BALL---\n please ask a question"
+  @response = gets.strip.downcase
+  if @response == "quit"
     puts "thank you come again"
-    exit
+  elsif @easter_egg.has_key?(@response)
+    puts @easter_egg[@response]
+    main_page
+  else
+    the_answer
   end
-  main_page
+  # case response
+  # when
+  #     puts "ask question"
+  #     ques = gets.strip
+  #     the_answer
+  # when "2"
+  #     add
+  # when "4"
+  #   puts @answers + @user_answers
+  # when "quit"
+  #   puts "thank you come again"
+  #   exit
+  # end
+  # main_page
 end
 
 main_page
