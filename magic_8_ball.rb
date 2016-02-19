2b85ebc584589272ab6e284700b6223506700403
Gavyn Caldwell <gavyncaldwell@gmail.com>
2016-02-18 17:34:26 -0700

    you can see history and add answers

--- magic_8_ball.rb
+++ magic_8_ball.rb
@@ -1,30 +1,47 @@
-@answers = ["well, duh....", "Dont ask me that", "are you kidding? NO!", "too high to think, ask again"]
-@main = "---WELCOME TO MAGIC 8 BALL---\n Please ask a question!"
+#need to finish with resetting the answers
 
-def restart
-  puts 'would you like to restart? (y/n)'
-  y_n = gets.strip.downcase
-  if y_n == "y"
+@answers = ["well, duh....", "Dont ask me that", "are you kidding? NO!", "too high to think, ask again"]
+
+def add
+  puts "type a new answer"
+  new_answer = gets.strip.downcase
+    if @answers.include?(new_answer)
+      puts "that question already exists"
+      add
+    elsif
+      new_answer == "quit"
+      puts "thank you come again"
+      exit
+    else
+      @answers << "#{new_answer}\n"
     main_page
-  elsif y_n == "n"
-    "have a nice day"
-  else
-    puts "enter y or n"
-    restart
-  end
+    end
 end
 
 def the_answer
-  new_answer = @answers.shuffle
-  message = new_answer[0]
+  answer = @answers.shuffle
+  message = answer[0]
   puts message
+  main_page
 end
 
 def main_page
-  puts @main
-  response = gets.strip
-  the_answer
-  restart
+  puts "\n\n---WELCOME TO MAGIC 8 BALL---\n1. ask a question!\n2. add answer\n3. reset answers\n4. list of answers\ntype quit in any moment to exit magic 8 ball"
+  response = gets.strip.downcase
+  case response
+  when "1"
+      puts "ask question"
+      ques = gets.strip
+      the_answer
+  when "2"
+      add
+  when "4"
+    puts @answers
+  when "quit"
+    puts "thank you come again"
+    exit
+  end
+  main_page
 end
 
 main_page
